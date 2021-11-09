700c56a80703ae31b509c713b7f46da8aebccf3c
Mike <arvene241@gmail.com>
2021-11-09 14:26:27 +0800

    fixed some errors

--- public/js/span-youthden.js
+++ public/js/span-youthden.js
@@ -1,16 +1,18 @@
 const yd_top = document.querySelector(".youth-den-p .top");
 const yd_bottom = document.querySelector(".youth-den-p .bottom");
 
-for (let index = 0; index < 10; index++) {
+for (let index = 0; index < 3; index++) {
     var span = document.createElement("span");
     var text = document.createTextNode("youthden.");
+    span.classList.add("color");
     span.appendChild(text);
     yd_top.appendChild(span);
 }
 
-for (let index = 0; index < 10; index++) {
+for (let index = 0; index < 3; index++) {
     var span = document.createElement("span");
     var text = document.createTextNode("youthden.");
+    span.classList.add("color");
     span.appendChild(text);
     yd_bottom.appendChild(span);
 }
