f0b114676bca52322a14e9b770daa5706cc2b3a7
Mike <arvene241@gmail.com>
2021-12-10 14:13:48 +0800

    fix transition and links

--- cart.php
+++ cart.php
@@ -32,7 +32,7 @@
                                 <a class="nav-link" href="about.php">about</a>
                             </li>
                             <li class="nav-item">
-                                <a class="nav-link" href="">faq</a>
+                                <a class="nav-link" href="faqs.php">faq</a>
                             </li>
                             <div class="sign-in">
                                 <button class="btn-signIn"><a href="sign-up.php">sign in</a></button>
@@ -128,61 +128,7 @@
         </div>
     </section>
 
-    <!-- FOOTER -->
-    <footer>
-        <div class="container">
-            <div class="footer">
-                <div class="newsletter">
-                    <h3>newsletter</h3>
-                    <p>Subscribe To Our Newsletter To Receive Our News</p>
-                    <div class="email">
-                        <input type="email" name="email" id="" placeholder="Your email address">
-                        <a href="">subscribe</a>
-                    </div>
-                </div>
-                <div class="nav">
-                    <ul class="menu">
-                        <li class="nav-item">
-                            <a class="nav-link" href="index.php">home</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="">shop</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="about.php">about</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="">faq</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="socials">
-                    <ul class="menu">
-                        <li class="social-item">
-                            <a class="social-link" href="https://www.facebook.com/" target="_blank">facebook</a>
-                        </li>
-                        <li class="social-item">
-                            <a class="social-link" href="https://www.twitter.com/" target="_blank">twitter</a>
-                        </li>
-                        <li class="social-item">
-                            <a class="social-link" href="https://www.instagram.com/" target="_blank">instagram</a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="yd-contact">
-                    <div class="footer-logo">
-                        <a href="#"><img src="public/images/logo/logo-arvene-ver.png" alt="logo"></a>
-                    </div>
-                    <div class="contacts">
-                        <p>Youth Den Store, No.101 Lipa City, Malvar, Batangas</p>
-                        <p>Youthden@gmail.com</p>
-                        <p>+234-818-134-6000</p>
-                    </div>
-                </div>
-            </div>
-            <p class="all-rights">© 2021 youthden. All rights reserved</p>
-        </div>
-    </footer>
+    <?php include 'footer.php' ?>
 
     <script src="public/js/hamburger.js"></script>
 
