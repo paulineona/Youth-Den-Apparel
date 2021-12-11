c07bcdc0edb79ef2468ffb1a81bb86e34a615987
Mike <arvene241@gmail.com>
2021-12-11 13:35:41 +0800

    update database

--- database/youthden_ecommerce.sql
+++ database/youthden_ecommerce.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2021 at 06:30 PM
+-- Generation Time: Dec 11, 2021 at 06:34 AM
 -- Server version: 10.4.19-MariaDB
 -- PHP Version: 8.0.6
 
@@ -20,6 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `youthden_ecommerce`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_accounts`
+--
+
+CREATE TABLE `tbl_accounts` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `first_name` varchar(200) NOT NULL,
+  `last_name` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tbl_accounts`
+--
+
+INSERT INTO `tbl_accounts` (`id`, `first_name`, `last_name`, `email`, `password`) VALUES
+(1, 'midoriya', 'izuku', 'midoriya.izuku@gmail.com', '12345'),
+(2, 'ichigo', 'kurosaki', 'ichigo.kurosaki@gmail.com', '12222'),
+(3, 'mike', 'izuku', 'mike@gmail.com', '234');
 
 -- --------------------------------------------------------
 
@@ -63,9 +86,8 @@ CREATE TABLE `tbl_products` (
   `id` int(10) UNSIGNED NOT NULL,
   `product_name` varchar(100) NOT NULL,
   `product_price` float NOT NULL,
-  `description` varchar(255) NOT NULL,
+  `description` longtext NOT NULL,
   `img_name` varchar(150) NOT NULL,
-  `featured` varchar(10) NOT NULL,
   `stock` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -73,16 +95,34 @@ CREATE TABLE `tbl_products` (
 -- Dumping data for table `tbl_products`
 --
 
-INSERT INTO `tbl_products` (`id`, `product_name`, `product_price`, `description`, `img_name`, `featured`, `stock`) VALUES
-(1, 'Coffee Club', 550, 'coffee is life', '6198cb60cdf414.69797380.png', 'Yes', 20),
-(2, 'No Fear', 650, 'no fear', '6198cdad0d6375.56007800.png', 'No', 30),
-(3, 'Euphoria', 700, 'Euphoria', '6198ceceb940b4.25893407.png', 'Yes', 50),
-(4, 'Too High', 650, 'Too High', '6198cf004d3357.87151959.png', 'No', 45),
-(5, 'Plants & Music', 500, 'Too High', '6198cf27b66551.49623927.png', 'Yes', 43),
-(6, 'Zimer', 750, 'Zimer', '61a05c950cec12.64504695.png', 'Yes', 50),
-(8, 'Placebo', 550, 'Placebo', '61a0f99c56ae03.94378719.png', 'Yes', 70),
-(9, 'Vice', 650, 'Vice is limited edition', '61ae206ee4c9a7.52225897.png', 'No', 25),
-(11, 'Vinyl', 550, 'Vinyl', '61ae3f195e8d46.85787570.png', 'No', 155);
+INSERT INTO `tbl_products` (`id`, `product_name`, `product_price`, `description`, `img_name`, `stock`) VALUES
+(1, 'Coffee Club', 450, 'Now you can wait for your morning coffee wearing this t-shirt! Don\'t wait! Oh, wait...\r\n\r\nMade of 100% combed cotton\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '6198cb60cdf414.69797380.png', 20),
+(2, 'No Fear', 600, 'No Fear! No Worry! No Control!\r\n\r\nMade of 100% combed cotton\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '6198cdad0d6375.56007800.png', 30),
+(3, 'Euphoria', 700, 'eu·pho·ri·a - a feeling or state of intense excitement and happiness.\r\n\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '6198ceceb940b4.25893407.png', 50),
+(4, 'Too High', 700, 'I’m too high to sleep.\r\n\r\n100% Combed Cotton\r\nCOLD WASH ONLY\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '6198cf004d3357.87151959.png', 45),
+(5, 'Plants Like Music Too', 450, 'Plants thrive when they listen to music that sits between 115Hz and 250Hz.\r\n\r\nMade of 100% combed cotton\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '6198cf27b66551.49623927.png', 43),
+(6, 'Manifestation', 600, 'Manifestation - the action or fact of showing an abstract idea.\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61a05c950cec12.64504695.png', 50),
+(8, 'Placebo', 600, 'pla·ce·bo - a measure designed merely to calm or please someone.\r\n\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61a0f99c56ae03.94378719.png', 70),
+(9, 'Vice Cigarette', 450, 'A cigarette a day keeps the doctor in pay.\r\n\r\nMade of 100% combed cotton\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61ae206ee4c9a7.52225897.png', 250),
+(11, 'Vinyl Junkie', 650, 'Nothing sounds sweeter than the crackle of vinyl.\r\n\r\n100% Combed Cotton\r\nCOLD WASH ONLY\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61ae3f195e8d46.85787570.png', 155),
+(12, 'Good Boys Club', 650, 'Come join the hottest club in town!\r\n\r\nMade of 100% combed cotton\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61b055fe3d6d80.31816983.png', 300),
+(13, 'Ethernal Hell', 700, 'Is Hell real? Is Hell eternal?\r\n\r\nMade of 100% combed cotton\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61b43114016a26.71909231.png', 150),
+(14, 'Let\'s Dance', 450, 'Come on! Join us and dance the night away.\r\n\r\n100% Combed Cotton\r\n\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61b43321977601.93003832.png', 155),
+(15, 'Summer Intern', 550, 'Keep calm and be a summer intern. \r\n\r\nMade of 100% combed cotton\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61b4334ee1e7b9.06208100.png', 200),
+(16, 'Nankinmushi', 900, 'Japanese Influential Design in Black Tee. Shipping Worldwide.\r\n\r\n\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61b4337fb8f1e2.17810637.png', 70),
+(17, 'Hotdog Club', 550, 'Either you love hotdogs or you’re wrong.\r\n\r\nMade of 100% combed cotton\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61b433a3089633.67506914.png', 155),
+(18, 'Palawan Hot', 550, 'It’s always hot in Palawan.\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry \r\n', '61b433c4bec319.70134614.png', 200),
+(19, 'Allergic Club', 550, 'What doesn’t kill you makes you feel worse.\r\n\r\nMade of 100% combed cotton\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61b4343921a415.29825113.png', 150),
+(20, 'Badass Baller', 650, 'Become a badass baller to win the game.\r\n\r\n Made of 100% combed cotton\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry. \r\n', '61b4345c236e84.70487039.png', 70),
+(21, 'Create Art', 600, 'Just keep creating art. \r\n\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61b4346ea54d68.00778497.png', 150),
+(22, 'Disconnected', 600, 'Disconnect with the world, reconnect with yourself.\r\n\r\nMade of 100% combed cotton\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61b4348bdc1b10.25826565.png', 20),
+(23, 'Good Ol\' Spooks', 600, 'If there\'s something strange\r\nIn your neighborhood\r\nWho you gonna call?\r\n\r\nMade of 100% combed cotton\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61b434cbcf1f88.49122792.png', 200),
+(24, 'Leaning Tower of Pizza', 550, 'You can’t buy happiness but you can buy a leaning tower of pizza thou.\r\n\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61b434e3a7d139.26555866.png', 150),
+(25, 'Magical Mystery Drops', 550, 'Lost in love? One drop, fixes all!\r\n\r\nMade of 100% combed cotton\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61b434fb1e4016.07634443.png', 266),
+(26, 'No Sea', 600, 'e sea isn’t real.\r\n\r\n\r\nMade of 100% combed cotton\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61b43512a8b0b2.17237779.png', 200),
+(27, 'Plant Bass', 500, 'A compassionate lifestyle through vegan food, music, art and community\r\n\r\n\r\n100% Combed Cotton\r\nCOLD WASH ONLY\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61b43529187270.65520575.png', 242),
+(28, 'The Pool Club', 600, 'Home is where the pool is. Join the pool club to feel at home.\r\n\r\n\r\n\r\n100% Combed Cotton\r\nCOLD WASH ONLY\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61b43543ad0eb0.45085503.png', 241),
+(29, 'No Plans', 550, 'No plans. No goals. No disappointments.\r\n\r\n100% Combed Cotton\r\nCOLD WASH ONLY\r\n*Machine wash cold\r\n*Dry cleanable\r\n*Do not iron on print\r\n*Do not bleach\r\n*Do not wring\r\n*Do not tumble dry\r\n', '61b43556e4dcb2.28331791.png', 135);
 
 -- --------------------------------------------------------
 
@@ -114,6 +154,12 @@ INSERT INTO `tbl_staff` (`id`, `name`, `email`, `date`, `role`) VALUES
 --
 
 --
+-- Indexes for table `tbl_accounts`
+--
+ALTER TABLE `tbl_accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `tbl_customer`
 --
 ALTER TABLE `tbl_customer`
@@ -142,6 +188,12 @@ ALTER TABLE `tbl_staff`
 --
 
 --
+-- AUTO_INCREMENT for table `tbl_accounts`
+--
+ALTER TABLE `tbl_accounts`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `tbl_customer`
 --
 ALTER TABLE `tbl_customer`
@@ -157,13 +209,13 @@ ALTER TABLE `tbl_orders`
 -- AUTO_INCREMENT for table `tbl_products`
 --
 ALTER TABLE `tbl_products`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `tbl_staff`
 --
 ALTER TABLE `tbl_staff`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
