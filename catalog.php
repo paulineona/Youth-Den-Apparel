dcb9fadd88681d6b3f03d644b6b1d7c8f48a1079
kristiaisabelle <kristiasamson@gmail.com>
2021-12-14 16:59:54 +0800

    add to cart backend & ui update

--- catalog.php
+++ catalog.php
@@ -9,146 +9,47 @@
     }
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Shop</title>
-
-    <link rel="icon" type="image/png" href="public/images/logo/logo-arvene-ver.png" />
-    <link rel="stylesheet" href="public/css/catalog.css" />
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-</head>
-
-<body>
-    <!-- HEADER -->
-    <header>
-        <!-- NAV -->
-        <nav class="sticky">
-            <div class="container navbar">
-                <div class="navbar-left">
-                    <div class="navbar-logo">
-                        <a href="index.php"><img src="public/images/logo/logo-arvene-ver.png" alt="logo" /></a>
-                    </div>
-                    <div class="overlay">
-                        <ul class="menu">
-                            <li class="nav-item">
-                                <a class="nav-link" href="index.php">home</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="catalog.php">shop</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="about.php">about</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="faqs.php">faq</a>
-                            </li>
-                            <div class="sign-in">
-                                <button class="btn-signIn">
-                                    <a href="sign-up.php">sign in</a>
-                                </button>
-                            </div>
-                        </ul>
-                    </div>
-                </div>
-                <div class="navbar-right">
-                    <div class="navbar-button icon1">
-                        <a href="cart.php"></a>
-                    </div>
-                    <div class="navbar-button icon2">
-                        <a href="sign-up.php"></a>
-                    </div>
-                    <div class="navbar-button hamburger"></div>
-                </div>
-            </div>
-        </nav>
-    </header>
-
-    <!-- CATALOG-->
-    <section class="shop">
-        <div class="container">
-            <div id="title">Shop</div>
-            <div id="subtitle">t-shirts</div>
-            <div class="cards">
             <?php
-                $count_query = "SELECT count(*) as allcount FROM tbl_products";
+                $count_query = "SELECT count(*) FROM tbl_products";
                 $count_result = $con->query($count_query);
                 $count_fetch = mysqli_fetch_array($count_result);
-                $postCount = $count_fetch['allcount'];
-                $limit = 6;
+                $postCount = $count_fetch;
+                $limit = 7;
     
                 $query = "SELECT * FROM `tbl_products` ORDER BY `id` ASC LIMIT 0, " . $limit;  
                 $result = $con->query($query);
             
                 if ($result->num_rows > 0) {
-                    while($row = $result->fetch_array()){ 
-                        echo "<div class=\"card\">";
-                            $img = "admin/images/" . $row['img_name'];
-                            echo "<img src=$img alt=\"\" class=\"card__image\" />";
-                            echo "<div class=\"card__content\">";
-                                echo "<p class=\"name\">" . $row['product_name'] . "</p>";
-                                echo "<p class=\"price\">Php. " . $row['product_price'] . ".00</p>";
-                                echo "<button class=\"addtocart\"><a href=\"cart.php\">add to cart</a></button>";
-                            echo "</div>";
-                        echo "</div>";
+                    while($row = $result->fetch_array()){
+
+                //insert
+                    $output = " ";
+
+                    if (mysqli_num_rows($result) < 1) {
+                        $output .= "No Item";
                     }
-                }
+                    while ($row = mysqli_fetch_array($result)) { 
 
-            ?>
-            </div>
-            <div class="shop">
-                <input type="button" class="btn-shop" id="loadBtn" value="Load More">
-                <input type="hidden" id="row" value="0">
-                <input type="hidden" id="postCount" value="<?php echo $postCount; ?>">
-            </div>
-        </div>
-    </section>
+                        $output .= "
+                        
+                        <div class='card'>
+                                <img class='card__image' src='admin/images/".$row['img_name']."' width='100%' height='auto'>
+                            <div class='card__content'>
+                                <p class='name'>".$row['product_name']."</p>
+                                <p class='price'>Php ".$row['product_price'].".00</p>
+                                <input type='hidden' name='product_name' id='product_name".$row['id']."' value=".$row['product_name'].">
+                                <input type='hidden' name='product_price' id='product_price".$row['id']."' value=".$row['product_price'].">
+                                <label for='quantity'>Quantity: </label>
+                                <input type='number' class='quantity'name='quantity' id='quantity".$row['id']."' value='1'>
 
-    <?php include 'includes/footer.php' ?>
+                                <input type='submit' name='addtocart' class='addtocart' value='Add to Bag' id='".$row['id']."'>
+                            </div>
+                       </div> ";
+                    }
 
-    <?php
-        closeCon($con); // close connection
-    ?>
+                    echo $output;
 
-    <script src="public/js/hamburger.js"></script>
+                    ?>
 
-    <script>
-      $(document).ready(function () {
-        $(document).on('click', '#loadBtn', function () {
-          var row = Number($('#row').val());
-          var count = Number($('#postCount').val());
-          var limit = 6;
-          row = row + limit;
-          $('#row').val(row);
-          $("#loadBtn").val('Loading...');
-        
-          $.ajax({
-            type: 'POST',
-            url: 'loadmore-data.php',
-            data: 'row=' + row,
-            success: function (data) {
-              var rowCount = row + limit;
-              console.log(rowCount);
-                console.log(count);
-              $('.cards').append(data);
-              if (rowCount >= count) {
-                $('#loadBtn').css("display", "none");
-                $('.btn-shop').css("display", "none");
-              } else {
-                $("#loadBtn").val('Load More');
-              }
-            }
-          });
-        });
-      });
-    </script>
-
-</body>
-
-</html>
+            <?php  }
+            } ?>
--- loadmore-data.php
+++ loadmore-data.php
@@ -5,25 +5,47 @@
 
     if (isset($_POST['row'])) {
         $start = $_POST['row'];
-        $limit = 6;
+        $limit = 7;
         $query = "SELECT * FROM `tbl_products` ORDER BY `id` ASC LIMIT ".$start.",".$limit;
         $result = $con->query($query);
 
         if ($result->num_rows > 0) {
-            while ($row = $result->fetch_array()) {
-                echo "<div class=\"card\">";
-                    $img = "admin/images/" . $row['img_name'];
-                    echo "<img src=$img alt=\"\" class=\"card__image\" />";
-                    echo "<div class=\"card__content\">";
-                        echo "<p class=\"name\">" . $row['product_name'] . "</p>";
-                        echo "<p class=\"price\">Php. " . $row['product_price'] . ".00</p>";
-                        echo "<button class=\"addtocart\"><a href=\"cart.php\">add to cart</a></button>";
-                    echo "</div>";
-                echo "</div>";
-            }
+            while ($row = $result->fetch_array()) { ?>
+
+
+                    <?php 
+
+                    $output = " ";
+
+                    if (mysqli_num_rows($result) < 1) {
+                        $output .= "No Item";
+                    }
+                    while ($row = mysqli_fetch_array($result)) { 
+
+                        $output .= "
+                        
+                        <div class='card'>
+                                <img class='card__image' src='admin/images/".$row['img_name']."' width='100%' height='auto'>
+                            <div class='card__content'>
+                                <p class='name'>".$row['product_name']."</p>
+                                <p class='price'>Php ".$row['product_price'].".00</p>
+                                <input type='hidden' name='product_name' id='product_name".$row['id']."' value=".$row['product_name'].">
+                                <input type='hidden' name='product_price' id='product_price".$row['id']."' value=".$row['product_price'].">
+                                <label for='quantity'>Quantity: </label>
+                                <input type='number' class='quantity'name='quantity' id='quantity".$row['id']."' value='1'>
+
+                                <input type='submit' name='add_to_bag' class='addtocart' value='Add to Bag' id='".$row['id']."'>
+                            </div>
+                       </div> ";
+                    }
+                    echo $output;
+
+                    ?>
+            
+           <?php  }
         }
     }
-?>
+            ?> 
 
 <?php
     closeCon($con); // close connection
